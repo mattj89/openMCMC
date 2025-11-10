3d188d4d6ca4abf309dabbf3f5c2fd133b2b0607
matthew.jones <matthew.m.jones2@shell.com>
2025-11-10 14:21:56 +0100

    Updates to all docstrings.

--- src/openmcmc/distribution/distribution_jax.py
+++ src/openmcmc/distribution/distribution_jax.py
@@ -22,7 +22,18 @@ from openmcmc import gmrf
 
 @dataclass
 class Distribution_jax(ABC):
-    """Abstract distribution class for the JAX case."""
+    """Abstract distribution class for the JAX case.
+
+    Attributes:
+        response (str): name of the response variable in the state dictionary.
+        grad_list (list): list of parameter names for which gradients will be computed.
+        param_list (list): list of all parameters in the conditioning set for the distribution.
+        grad_functions (dict): dictionary of gradient functions with respect to each individual parameter in
+            self.grad_list
+        hessian_functions (dict): dictionary of hessian functions with respect to each individual parameter in
+            self.grad_list.
+
+    """
 
     response: str
     grad_list: list
@@ -43,17 +54,25 @@ class Distribution_jax(ABC):
 class Normal_jax(Distribution_jax):
     """Normal distribution with options to jit compile and use grad functionality from JAX.
 
+    If self.jit_compile is True, a compiled version of the log-density function is created during initialisation and
+    stored in self.log_p_jit. This version is then used for all future evaluations.
+
     Attributes:
+        mean (LinearCombination_jax): mean parameter for the Normal distribution.
+        precision (LinearCombination_jax): precision matrix parameter for the Normal distribution. TODO (07/11/25): this
+            is currently unused- we just use a scalar instead.
+        jit_compile (bool): whether to JIT compile the log-density function.
+        scalar_precision (float): scalar precision value to use in place of a full precision matrix.
+        domain_response_lower (Union[float, None]): lower truncation limit for the response variable.
+        domain_response_upper (Union[float, None]): upper truncation limit for the response variable.
 
     """
     mean: LinearCombination_jax
     precision: LinearCombination_jax
     jit_compile: bool = True
-    fixed_precision: bool = True
     scalar_precision: float = 1.0
-    log_det_precision: float = 1.0
-    domain_response_lower: np.ndarray = None
-    domain_response_upper: np.ndarray = None
+    domain_response_lower: Union[float, None] = None
+    domain_response_upper: Union[float, None] = None
 
     def __post_init__(self):
         """Post intialisation: set up the JAX gradient."""
@@ -64,17 +83,23 @@ class Normal_jax(Distribution_jax):
     def log_p_internal(self, state: dict, update_index: int = None) -> Tuple[jnp.ndarray, dict]:
         """Evaluate the log-posterior distribution.
 
-        TODO: update_index set to default of 0 at the moment. Possible to have the default as nothing getting updated?
+        This is the internal definition of the log-density function. If self.jit_compile is True, this function will be
+        JIT compiled and stored in self.log_p_jit.
+
+        NOTE (07/11/25): this is currently set up to simply use a scalar precision value, for speed & to avoid having
+        to combine JAX and scipy.sparse operations. Should we re-do this to include JAX sparse functionality?
 
         Args:
-            state (dict): dictionary containing current parameter infotmation.
-            update_state (bool): flag indicating whether a state update is required (in cases where dependent parameters
-                in the state will also change).
+            state (dict): dictionary containing current parameter information.
+            update_index (int): index of the parameter to be updated. This information is passed down to the mean
+                predictor, where it may inform which elements of a coupling matrix need to be updated.
+
+        Returns:
+            log_p (jnp.ndarray): log-posterior density value.
+            state (dict): updated state dictionary.
 
         """
         mean, state = self.mean.predictor(state, update_index=update_index)
-        # precision, state = self.precision.predictor(state)
-        # exponent_term = jnp.vdot(state[self.response] - mean, precision @ (state[self.response] - mean))
         exponent_term = jnp.vdot(state[self.response] - mean, state[self.response] - mean) * self.scalar_precision
         log_p = 0.5 * (state[self.response].shape[0] *
                         (jnp.log(self.scalar_precision) - jnp.log(2 * jnp.pi)) - exponent_term)
@@ -88,6 +113,19 @@ class Normal_jax(Distribution_jax):
 
     def log_p(self, state: dict, update_index: bool = None) -> Tuple[jnp.ndarray, dict]:
         """Evaluate the log-posterior distribution (jit compiled if requested).
+
+        If self.jit_compile is True, the JIT compiled version of the log-density function is used. Otherwise, the
+        non-compiled function self.log_p_internal is used.
+
+        Args:
+            state (dict): dictionary containing current parameter information.
+            update_index (int): index of the parameter to be updated. This information is passed down to the mean
+                predictor, where it may inform which elements of a coupling matrix need to be updated.
+
+        Returns:
+            log_p (jnp.ndarray): log-posterior density value.
+            state (dict): updated state dictionary.
+
         """
         if self.jit_compile:
             log_p, state = self.log_p_jit(state, update_index)
@@ -102,6 +140,9 @@ class Normal_jax(Distribution_jax):
         with respect to a specific sub-set of the state parameters- this avoids the need to compute gradients wrt all
         state variables (which would result in wasted computation).
 
+        Attaches self.grad_functions and self.hessian_functions as attributes of the class, which are dictionaries
+        containing the JIT-compiled gradient and hessian functions for each parameter in self.grad_list.
+
         """
         self.grad_functions = {}
         self.hessian_functions = {}
@@ -115,21 +156,40 @@ class Normal_jax(Distribution_jax):
             self.hessian_functions[param] = jit(hessian(temp_log_p, argnums=1), static_argnums=(2, 3))
 
     def grad_log_p(self, state: dict, param: str, update_index: int = None, hessian_required: bool = True) -> jnp.ndarray:
-        """Evaluate the gradient of the log-posterior distribution."""
+        """Evaluate the gradient of the log-posterior distribution.
+
+        The gradients & hessians are calculated using the pre-computed JAX grad functions stored in self.grad_functions.
+        Outputs are converted back to np.ndarray for compatibility with the rest of the codebase.
+
+        Args:
+            state (dict): dictionary containing current parameter information.
+            param (str): name of the parameter with respect to which the gradient is computed.
+            update_index (int): index of the parameter to be updated. This information is passed down to the mean
+                predictor, where it may inform which elements of a coupling matrix need to be updated.
+            hessian_required (bool): whether to also compute and return the hessian matrix.
+
+        Returns:
+            grad_log_p (np.ndarray): gradient of the log-posterior
+            hess_log_p (np.ndarray, optional): hessian of the log-posterior (if hessian_required is True).
+
+        """
         grad_log_p = self.grad_functions[param](state, state[param], update_index)
         grad_log_p = np.asarray(grad_log_p).reshape((state[param].size, 1))
         if hessian_required:
             hess_log_p = self.hessian_functions[param](state, state[param], update_index)
             hess_log_p = -np.asarray(hess_log_p).reshape((state[param].size, state[param].size))
-            # hess_log_p = np.diag(np.abs(np.diag(hess_log_p))) # TODO: better!
-            # hess_log_p = np.diag([1e1, 1e1, 1e2]) # fudge: just put something
+            # hess_log_p = np.diag(np.abs(np.diag(hess_log_p)))
+            # TODO (07/11/25): is there a better solution for the Hessian? Or should we just eliminate cases where this
+            # is required for now?
             return grad_log_p, hess_log_p
         else:
             return grad_log_p
 
     def conditional_precision(self, state: dict, param: str) -> np.ndarray:
         """get the conditional precision matrix for NormalNormal updates.
-        TODO (17/06/25): used to do this based on the grad
+        TODO (17/06/25): used to do this based on the grad.
+
+        TODO (07/11/25): is this even used any more?
         """
         precision, _ = self.precision.predictor(state)
         if isinstance(self.mean, LinearCombination_jax):
@@ -138,7 +198,17 @@ class Normal_jax(Distribution_jax):
         else:
             return np.asarray(precision)
 
-    def rvs(self, state: dict, n:int = 1) -> np.ndarray:
+    def rvs(self, state: dict, n: int = 1) -> np.ndarray:
+        """Generate a random sample from the specified distribution.
+
+        Args:
+            state (dict): dictionary containing current parameter information.
+            n (int): number of samples to generate.
+
+        Returns:
+            samples (np.ndarray): generated samples. shape=(p, n).
+
+        """
         mean, _ = self.mean.predictor(state)
         precision, _ = self.precision.predictor(state)
         return gmrf.sample_normal(mu=mean, Q=precision, n=n)
--- src/openmcmc/parameter_jax.py
+++ src/openmcmc/parameter_jax.py
@@ -21,18 +21,47 @@ class LinearCombination_jax(LinearCombination):
             update_index (int): index of the parameter to update. Defaults to None.
 
         Returns:
-            jnp.ndarray:
-            dict:
+            jnp.ndarray: predictor evaluated using the information in state.
+            dict: state dictionary with updated prefactor matrices.
+
         """
         state = self.update_prefactors(state, update_index=update_index)
         return self.predictor_conditional(state), state
 
+    def predictor_conditional(self, state, term_to_exclude = None):
+        """Overloaded version, to take account of the fact that the terms are being screened in/out by the RJ
+        indicator.
+
+        TODO (21/10/21): do we even need to overload this now?
+
+        Args:
+            state (dict): state dictionary.
+            term_to_exclude (Union[str, list], optional): term(s) to exclude from the linear combination.
+                Defaults to None.
+
+        Returns:
+            sum_terms (jnp.ndarray): linear combination evaluated using the information in state.
+
+        """
+        if term_to_exclude is None:
+            term_to_exclude = []
+
+        if isinstance(term_to_exclude, str):
+            term_to_exclude = [term_to_exclude]
+
+        sum_terms = 0
+        for prm, prefactor in self.form.items():
+            if prm not in term_to_exclude:
+                sum_terms += state[prefactor] @ state[prm]
+        return sum_terms
+
     @abstractmethod
-    def update_prefactors(self, state: dict) -> None:
+    def update_prefactors(self, state: dict, update_index: int) -> dict:
         """Method to update the prefactor matrices of the linear combincation.
 
         Args:
             state (dict): state dictionary.
+            update_index (int): indices of e.g. the coupling matrix columns to update.
 
         Returns:
             state (dict): state dictionary with updated prefactor matrices.
